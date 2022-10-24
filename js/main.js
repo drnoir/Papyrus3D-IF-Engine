398f29f2a87ec52b83bd99afc0a5bc71c3976ba9
drnoir <chrispgodber@gmail.com>
2022-10-24 17:14:20 +0100

    - developing ui - added texures

--- js/main.js
+++ js/main.js
@@ -5,78 +5,72 @@
 let config;
 let chars;
 let diag;
+let textures;
 
-initLoad();
+loadData();
 
-// load JSON data
-async function initLoad() {
-    await loadData();
-    await populateScene();
-}
-
-function loadData(){
+async function loadData(){
 // game vars
-    config = loadConfig();
-    chars = loadChars();
-    diag = loadDiag();
-    console.log(config, chars, diag);
+   await loadConfig();
+   await loadChars();
+   await  loadDiag();
+   await  populateScene();
+   console.log(typeof  config, chars, diag);
 }
 
 async function loadConfig() {
-    await fetch("Game/config.json")
-        .then((response) => {
-            return response.json().then((data) => {
-                console.log(data);
-                return data;
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-}
-async function loadChars() {
-    await fetch("Game/charecters.json")
-        .then((response) => {
-            return response.json().then((data) => {
-                console.log(data);
-                return data;
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
-}
-async function loadDiag() {
-    await fetch("Game/dialogue.json")
-        .then((response) => {
-            return response.json().then((data) => {
-                console.log(data);
-                return data;
-            }).catch((err) => {
-                console.log(err);
-            })
-        });
+    const res = await fetch('Game/config.json')
+    config = await res.json();
 }
 
+async function loadChars() {
+    const res = await fetch('Game/charecters.json')
+    chars = await res.json();
+}
+
+async function loadDiag() {
+    const res = await fetch('Game/dialogue.json')
+    diag = await res.json();
+}
+
+// populate scene function
 function populateScene(){
-    console.log('testing data at load'+ chars);
     // load assets
-    let scene = document.querySelector('a-frame')
-    let assets = document.createElement('a-assets')
+    let scene = document.querySelector('a-scene')
+    let assets = document.createElement('a-assets');
     scene.appendChild(assets);
     // add character info and model path
-    chars.forEach(char=> assets.appendChild(char));
+    for (let i = 0; i < chars.characters.length; i++) {
+        let assetItem = document.createElement('a-asset-item');
+        assetItem.setAttribute('id',chars.characters[i].id)
+        assetItem.setAttribute('src',chars.characters[i].model)
+        assets.appendChild(assetItem);
+
+        // // add charecters to scene
+        let modelRef = '#'+chars.characters[i].id;
+        console.log(modelRef);
+        let char = document.createElement('a-entity');
+        char.setAttribute('name', chars.characters[i].name);
+        char.setAttribute('gltf-model', modelRef);
+        char.setAttribute('position', '0 0 -5');
+        char.setAttribute('scale', 1,1 ,1);
+        scene.appendChild(char);
+    }
 }
 
+// function to move to next scene
 function nextScene(){
 
 }
 
+// function to clear scene
 function clearScene(){
 
 }
 
+// create room function
 function createRoom(roomType, roomWidth, roomHeight, roomDepth){
     let roomArea = roomWidth * roomDepth;
-
     switch(roomType) {
         case 'indoor':
             // code block
@@ -87,6 +81,4 @@ function createRoom(roomType, roomWidth, roomHeight, roomDepth){
         default:
         // code block
     }
-
-
 }
