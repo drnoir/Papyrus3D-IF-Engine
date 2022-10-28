c6db7fea03d145c9a08d49549bbbd3ba66460b0d
drnoir <chrispgodber@gmail.com>
2022-10-28 18:58:51 +0100

    - building componenets - glow, charecter etc - testing scene etc - asset updates - directory restruct

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 // main js
-
 // global game vars
 let config;
 let chars;
 let diag;
 let textures;
 let currentDiagID = 0;
+let currentScene = 1;
 
 // dialogueUI Elements
-const scene = document.querySelector('a-scene')
+const scene = document.querySelector('a-scene');
+const assets = document.querySelector('a-assets');
 const dialogueUI = document.getElementById("dialogueID");
-
 loadData();
 
 async function loadData(){
 // game vars
    await loadConfig();
    await loadChars();
-   await loadDiag();
+   await loadDiag(1);
    await populateScene();
-   // testing dialogue UI population
-   populateDiag(0);
-   console.log(typeof  config, chars, diag);
+   await populateDiag(0)
+   // testing dialogue.json UI population
+   console.log(config, chars, diag);
 }
 
 async function loadConfig() {
@@ -34,16 +34,15 @@ async function loadChars() {
     chars = await res.json();
 }
 
-async function loadDiag() {
-    const res = await fetch('Game/dialogue.json')
+async function loadDiag(sceneToLoad) {
+    let fetchURL = 'Game/scenes/scene'+sceneToLoad+'/dialogue.json';
+    const res = await fetch(fetchURL)
     diag = await res.json();
 }
 
+
 // populate scene function
 function populateScene(){
-    // load assets
-    let assets = document.createElement('a-assets');
-    scene.appendChild(assets);
     // add character info and model path
     for (let i = 0; i < chars.characters.length; i++) {
         let assetItem = document.createElement('a-asset-item');
@@ -52,15 +51,17 @@ function populateScene(){
         assets.appendChild(assetItem);
 
         // // add charecters to scene
-        let modelRef = '#'+chars.characters[i].id;
+        let modelRef = chars.characters[i].id;
+        let modelID = '#'+modelRef;
         console.log(modelRef);
         let char = document.createElement('a-entity');
         char.setAttribute('name', chars.characters[i].name);
-        char.setAttribute('gltf-model', modelRef);
+        char.setAttribute('gltf-model', modelID );
         char.setAttribute('position', '0 0 -5');
         char.setAttribute('scale', 1,1 ,1);
         scene.appendChild(char);
     }
+
 
     createSquareRoom();
 }
@@ -112,21 +113,50 @@ function createRoom(roomType, roomWidth, roomHeight, roomDepth){
 function createSquareRoom(textureWall, textureFloor){
     let wall1= document.createElement('a-box');
     let wall2= document.createElement('a-box');
+    let wallSide1= document.createElement('a-box');
+    let wallSide2= document.createElement('a-box');
+    let floor= document.createElement('a-plane');
+
+
+    floor.setAttribute('position', '0 0 0');
+    floor.setAttribute('rotation', '-90 0 0');
+    floor.setAttribute('width', '20');
+    floor.setAttribute('height', '20');
+    floor.setAttribute('material', 'src', '#grunge');
 
     wall1.setAttribute('position', '0 2 10');
     wall1.setAttribute('rotation', '0 0 0');
     wall1.setAttribute('width', '20');
     wall1.setAttribute('height', '5');
-    wall1.setAttribute('color', 'lightblue');
+    wall1.setAttribute('color', 'white');
+    wall1.setAttribute('material', 'src', '#brick');
 
     wall2.setAttribute('position', '0 2 -10');
     wall2.setAttribute('rotation', '0 0 0');
     wall2.setAttribute('width', '20');
     wall2.setAttribute('height', '5');
-    wall2.setAttribute('color', 'lightblue');
+    wall2.setAttribute('color', 'white');
+    wall2.setAttribute('material', 'src', '#brick');
 
+    wallSide1.setAttribute('position', '9 2 0.5');
+    wallSide1.setAttribute('rotation', '0 90 0');
+    wallSide1.setAttribute('width', '20');
+    wallSide1.setAttribute('height', '5');
+    wallSide1.setAttribute('color', 'white');
+    wallSide1.setAttribute('material', 'src', '#brick');
+
+    wallSide2.setAttribute('position', '-9.5 2 0.4');
+    wallSide2.setAttribute('rotation', '0 90 0');
+    wallSide2.setAttribute('width', '20');
+    wallSide2.setAttribute('height', '5');
+    wallSide2.setAttribute('color', 'white');
+    wallSide2.setAttribute('material', 'src', '#brick');
+
+    scene.appendChild(floor);
     scene.appendChild(wall1);
     scene.appendChild(wall2);
+    scene.appendChild(wallSide1);
+    scene.appendChild(wallSide2);
 }
 
 // UI functions - functions and actions for UI
@@ -145,13 +175,13 @@ function addButton() {
         nextPassageBtn.setAttribute('height', '0.15');
         nextPassageBtn.setAttribute('width', '0.15');
         nextPassageBtn.setAttribute('material', 'color: red');
-        nextPassageBtn.setAttribute('position', '0.2 1.2  0.1');
+        nextPassageBtn.setAttribute('position', '0.2 1.6 0.1');
         // addtext
         let nextPassageBtnTxt = document.createElement('a-text');
         nextPassageBtnTxt.setAttribute('value', '>');
         nextPassageBtnTxt.setAttribute('height', '1');
         nextPassageBtnTxt.setAttribute('width', '3');
-        nextPassageBtnTxt.setAttribute('position', '-0.05 0.011 0.1');
+        nextPassageBtnTxt.setAttribute('position', '-0.05 0.015 0.1');
         nextPassageBtn.appendChild(nextPassageBtnTxt);
 
         let bobGuy = document.getElementById('bobGuy') // FOR TESTING PURPOSES - needs to be passed associated char
