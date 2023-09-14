fe907b82a40e0d06eaff7cd5ddfbc2a520986c10
drnoir <chrispgodber@gmail.com>
2023-09-14 08:03:02 +0100

    - working on player placement resetting WIP

--- public/Tools/MapEditor/src/mapEditor.js
+++ public/Tools/MapEditor/src/mapEditor.js
@@ -132,7 +132,19 @@ AFRAME.registerComponent('editor-listener', {
                 }
             else  {
                 if (playerPlaceMode){
-                    alert('You have already placed the player start playerMarker, reset?');
+                    let txt;
+                    if (confirm("You have already placed the Player Start Position, Would you like to reset it?")) {
+                        const playerStartPos = document.getElementById('playerStart');
+                        playerStartPos.parentNode.removeChild(playerStartPos);
+                        updateMap(index, 0);
+                        txt = "You pressed OK, Player position can now be reset";
+                        // remove P from array and update with floor
+                        currentEntity === 'P';
+                      } else {
+                        txt = "You pressed Cancel, Player Position will remain in place";
+                        currentEntity === 0;
+                      }
+                  
             }
         }
             // door - not locked
@@ -614,4 +626,29 @@ slider.oninput = function () {
     output.innerHTML = this.value;
     custumHeightY = this.value;
     console.log('custum height Y'+ custumHeightY);
+}
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
